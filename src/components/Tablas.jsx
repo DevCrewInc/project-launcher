ed3b1644ebe8b8d84acc306fdcb29bf939f09ab1
Anna Maria Blanco <18annablanco@gmail.com>
2021-11-12 22:50:59 -0500

    ..en proceso título y botones

--- src/components/Tablas.jsx
+++ src/components/Tablas.jsx
@@ -4,8 +4,18 @@ import 'styles/globals.css';
 const Tablas = () => {
     return (
         <>
+            <div>
+            <i className = "fas fa-rocket "></i>
+            <span className = "font-bold">Proyectos</span>
+            </div>
+
+            <div className="flex">
+                <button className=" p-3 text-gray-400 hover:text-gray-800 ">Todos</button>
+                <button className=" p-3 text-gray-400 hover:text-gray-800 ">Mis proyectos</button>
+            </div>
+         
             <table className = "table-fixed border mx-14 my-5 text-center ">
-                <thead className = "bg-gray-200 ">
+                <thead className = "bg-gray-200 text-sm text-gray-700">
                     <tr className = "">
                         <th className = "p-1.5">Id</th>
                         <th className = "p-1.5">Nombre del proyecto</th>
@@ -18,13 +28,13 @@ const Tablas = () => {
                         <th className = "p-1.5">Acción</th>
                     </tr>
                 </thead> 
-                <tbody className = "">  
+                <tbody className = "text-sm text-gray-600">  
                     <tr className = "border">
                         <td className = "px-3">123</td>
                         <td className = "px-3">Proyecto de Química</td>
                         <td className = "px-3">12/11/2021</td>
                         <td className = "px-3">12/12/2021</td>
-                        <td className = "px-3">000</td>
+                        <td className = "px-3">0000000</td>
                         <td className = "px-3">Anna</td>
                         <td className = "px-3">En desarrollo</td>
                         <td >
@@ -36,21 +46,21 @@ const Tablas = () => {
                             <i className = "fas fa-trash my-1 p-1 text-gray-400 hover:text-gray-300  cursor-pointer"/>
                         </td>
                     </tr>
-                    <tr>
-                        <td>123</td>
-                        <td>Proyecto de Química</td>
-                        <td>12/11/2021</td>
-                        <td>12/12/2021</td>
-                        <td>000</td>
-                        <td>Anna</td>
-                        <td>En desarrollo</td>
-                        <td>
-                            <button>Activo</button>
+                    <tr className = "border">
+                        <td className = "px-3">123</td>
+                        <td className = "px-3">Proyecto de Química</td>
+                        <td className = "px-3">12/11/2021</td>
+                        <td className = "px-3">12/12/2021</td>
+                        <td className = "px-3">0000000</td>
+                        <td className = "px-3">Anna</td>
+                        <td className = "px-3">En desarrollo</td>
+                        <td >
+                            <button className = "colors mx-1 my-1 px-2">Activo</button>
                         </td>
-                        <td>
-                            <i className = "fas fa-eye "/>
-                            <i className = "fas fa-pen "/>
-                            <i className = "fas fa-trash"/>
+                        <td className = "flex">
+                            <i className = "fas fa-eye m-1 p-1 text-gray-400 hover:text-gray-300 cursor-pointer"/>
+                            <i className = "fas fa-pen my-1 p-1 text-gray-400 hover:text-gray-300  cursor-pointer"/>
+                            <i className = "fas fa-trash my-1 p-1 text-gray-400 hover:text-gray-300  cursor-pointer"/>
                         </td>
                     </tr>
                 </tbody>         
