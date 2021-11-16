acd930ce8aa5a70515266ac8f0508b1f1c6d7598
lilianaromerop <lilianaromerop11@gmail.com>
2021-11-16 08:28:43 -0500

    cambio rol a boton

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -6,7 +6,7 @@ const Usuarios = () => {
         <div className = "flex flex-col p-10">
         <div className="space-x-3">
             <i className = "fas fa-rocket fa-lg"></i>
-            <span className = "font-bold text-2xl">Proyectos</span>
+            <span className = "font-bold text-2xl">Usuarios</span>
         </div>
 
         <div className="my-4 space-x-2 mt-8 cursor-pointer">
@@ -40,16 +40,16 @@ const Usuarios = () => {
                     <td>
                         <input type="text" placeholder = "10293847" className ="input-tablas"/>
                     </td>
-                    <td>
-                        <input type="text" placeholder = "Rol" className ="input-tablas"/>
+                    <td className="text-center">
+                        <input className="text-gray-600 cursor-pointer bg-white" type="button" value="Estudiante"/>
                     </td>
                     <td className="text-center">
                         <button className = "status-button mx-1 my-1 px-2">Activo</button>
                     </td>
                     <td className = "flex justify-center space-x-2">
                         <i className = "fas fa-eye m-1 p-1 text-gray-400 hover:text-blue-600 cursor-pointer"/>
-                        <i className = "fas fa-pen my-1 p-1 text-gray-400 hover:text-yellow-400  cursor-pointer"/>
-                        <i className = "fas fa-trash my-1 p-1 text-gray-400 hover:text-red-400  cursor-pointer"/>
+                        <i className = "fas fa-pen my-1 p-1 text-gray-400 hover:text-yellow-400 cursor-pointer"/>
+                        <i className = "fas fa-trash my-1 p-1 text-gray-400 hover:text-red-400 cursor-pointer"/>
                     </td>
                 </tr>
             </tbody>         
